cf83bee3efe87b1e7647a46245c1c4f5b0c538ea
touqeer0011 <59115846+touqeer0011@users.noreply.github.com>
2020-09-13 21:28:05 +0500

    expense_tracker_app

---
+++ expense_tracker_app/src/child.js
@@ -0,0 +1,16 @@
+import React from 'react';
+function Child() {
+  return (
+    <div className="container">
+        <h1>Expense Tracker</h1>
+        <h3>Your Balance <br/>$260</h3>
+        <div className="expense_container">
+            <h3>INCOME<br/>$500</h3>
+            <h3>EXPENSE<br/>$240</h3>
+            </div> 
+    </div>
+
+  );
+}
+
+export default App;
